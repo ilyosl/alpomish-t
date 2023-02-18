101484d74537217cf1c108a3410696dd25869257
ilyosl <ilyosl@mail.ru>
2023-02-18 11:47:29 +0500

    adding files

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -1,12 +1,11 @@
 $(function (){
-    var title =
-    tippy('.alp-path', {
+    /*tippy('.alp-path', {
         content(reference) {
             const id = reference.getAttribute('data-template');
             const template = $(this);
             return template.innerHTML;
         },
-    });
+    });*/
 })
 
 
