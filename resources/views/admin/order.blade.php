733999671f060def3c4e429b2696499e5a54503f
ilyosl <ilyosl@mail.ru>
2023-02-20 17:41:02 +0500

    create blade

---
+++ resources/views/admin/order.blade.php
@@ -0,0 +1,9 @@
+    <div class="box box-solid box-info">
+        <div class="box-header with-border">
+            <h3 class="box-title">Продажа за сегодня</h3>
+        </div>
+        <div class="box-body">
+            <canvas id="typeListDay"></canvas>
+        </div>
+    </div>
+
